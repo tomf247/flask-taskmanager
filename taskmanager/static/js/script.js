f922ae573978c3ab49aa1eb63753c6454774e1da
tomf247 <tomfoley247@gmail.com>
2022-08-04 13:21:53 +0000

    Add Materialize Framework

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenave initializtion
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
